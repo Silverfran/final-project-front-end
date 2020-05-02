e0a3d685b479fe1c22bf9a1104b46652ad374902
SilverFran <Francisco1627@gmail.com>
2020-05-02 06:42:12 +0000

    sign in/up proveed

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const [user, setUser] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	return (
+		<div className="text-center mt-5">
+			<h1>Hello Rigo!</h1>
+			<div className="row justify-content-center bg-dark align-items-center">
+				<div className="col-sm-8 p-0">
+					<div className="row justify-content-center p-5 bg-light align-items-center">
+						<div className="col-sm-6">
+							<div className="card border-0">
+								<div className="card-body">
+									<h5 className="card-title">Sing in to Sys</h5>
+									<form>
+										<div className="form-group">
+											<label htmlFor="exampleInputEmail1">Email address</label>
+											<input
+												type="email"
+												className="form-control"
+												id="exampleInputEmail1"
+												aria-describedby="emailHelp"
+												placeholder="example@something.com"
+												onChange={e => setEmail(e.target.value)}
+											/>
+										</div>
+										<div className="form-group">
+											<label htmlFor="exampleInputPassword1">Password</label>
+											<input
+												type="password"
+												className="form-control"
+												id="exampleInputPassword1"
+												placeholder="******"
+												onChange={e => setPassword(e.target.value)}
+											/>
+										</div>
+										<button
+											type="button"
+											className="btn btn-primary"
+											onClick={() => console.log(email, password)}>
+											Sign In
+										</button>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div className="col-sm-6">
+							<div className="card border-0">
+								<div className="card-body">
+									<h5 className="card-title">Create new account</h5>
+									<form>
+										<div className="form-group">
+											<label htmlFor="formGroupExampleInput">Name</label>
+											<input
+												type="text"
+												className="form-control"
+												id="formGroupExampleInput"
+												placeholder="Jhon Doe"
+												onChange={e => setUser(e.target.value)}
+											/>
+										</div>
+										<div className="form-group">
+											<label htmlFor="exampleInputEmail2">Email address</label>
+											<input
+												type="email"
+												className="form-control"
+												id="exampleInputEmail2"
+												aria-describedby="emailHelp"
+												placeholder="example@something.com"
+												onChange={e => setEmail(e.target.value)}
+											/>
+										</div>
+										<div className="form-group">
+											<label htmlFor="exampleInputPassword2">Password</label>
+											<input
+												type="password"
+												className="form-control"
+												id="exampleInputPassword2"
+												placeholder="******"
+												onChange={e => setPassword(e.target.value)}
+											/>
+										</div>
+										<button
+											type="button"
+											className="btn btn-primary"
+											onClick={() => console.log(user, email, password)}>
+											Sign Up
+										</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<a href="#" className="btn btn-success">
+				If you see this green button, bootstrap is working
+			</a>
+		</div>
+	);
+};
