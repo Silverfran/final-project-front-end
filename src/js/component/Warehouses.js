27bbcb923ab1e13fff0f28ae30d671f05caced43
Nelson <nlparra.dev@gmail.com>
2020-06-03 01:27:29 +0000

    working on warehouses view

--- src/js/component/Warehouses.js
+++ src/js/component/Warehouses.js
@@ -1,5 +1,44 @@
 import React from "react";
 
 export const Warehouses = () => {
-	return "	Hello world";
+	return (
+		<div className="container p-5 ">
+			<h2>Warehouses</h2>
+			<div className="listofwarehouse overflow-auto">
+				<div className="row">
+					<div className="d-flex flex-row pl-4">
+						<div>
+							<img src="https://placehold.it/200x200" />
+						</div>
+						<div className="pl-5">
+							<h3>WAREHOUSE NAME</h3>
+							<p>lorem lorem lorem</p>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="d-flex flex-row pl-4">
+						<div>
+							<img src="https://placehold.it/200x200" />
+						</div>
+						<div className="pl-5">
+							<h3>WAREHOUSE NAME</h3>
+							<p>lorem lorem lorem</p>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="d-flex flex-row pl-4">
+						<div>
+							<img src="https://placehold.it/200x200" />
+						</div>
+						<div className="pl-5">
+							<h3>WAREHOUSE NAME</h3>
+							<p>lorem lorem lorem</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
